dd78e52cc61aee44715e2929fba17e1b863b3ef4
Anthony Maras <marasantonis@gmail.com>
2022-08-25 14:55:32 +0300

    initial setup for imported components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Cv from './Components/Cv/Cv';
+import Form from './Components/Form/Form';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Cv Generator</h1>
       </header>
-    </div>
+      <main>
+        <Form />
+        <Cv />
+      </main>
+      <footer>
+        <h2>footer</h2>
+      </footer>
+    </>
   );
 }
 
