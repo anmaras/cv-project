1f2d0e2ba54a9e085477b4d36476d78c63d4cb81
Anthony Maras <marasantonis@gmail.com>
2022-08-29 17:52:09 +0300

    create global custom context hook

--- src/Context.js
+++ src/Context.js
@@ -1,7 +1,9 @@
 import React, { useContext, useState } from 'react';
+import uuid from 'react-uuid';
 
 const AppContext = React.createContext();
-const initialState = {
+
+const personalData = {
   name: '',
   title: '',
   email: '',
@@ -10,37 +12,125 @@ const initialState = {
   linkedin: '',
   portfolio: '',
   profile: '',
-  yearFrom: '',
-  yearTo: '',
-  university: '',
-  subject: '',
-  degree: '',
-  position: '',
-  company: '',
-  companyCity: '',
-  workDescription: '',
-  workingFrom: '',
-  workingTo: '',
 };
 
-function Context({ children }) {
-  const [state, setState] = useState(initialState);
-  const [year, setYear] = useState(new Date().getFullYear());
+const eduFormListInitialValue = [
+  {
+    type: 'text',
+    name: ['university', 'degree', 'subject', 'yearFrom', 'yearTo'],
+    id: uuid(),
+    university: '',
+    subject: '',
+    degree: '',
+    yearFrom: '',
+    yearTo: '',
+  },
+];
+const expFormListInitialValue = [
+  {
+    type: 'text',
+    name: [
+      'position',
+      'company',
+      'companyCity',
+      'workDescription',
+      'workingFrom',
+      'workingTo',
+    ],
+    position: '',
+    company: '',
+    companyCity: '',
+    workDescription: '',
+    workingFrom: '',
+    workingTo: '',
+    id: uuid(),
+  },
+];
 
-  const onChangeHandler = (e) => {
+function Context({ children }) {
+  const [personalInfo, setPersonalInfo] = useState(personalData);
+  const [educationFormList, setEducationFormList] = useState(
+    eduFormListInitialValue
+  );
+  const [workXpFormList, setWorkXpFormList] = useState(expFormListInitialValue);
+
+  const personalInfoOnChangeHandler = (e) => {
     const { name, value } = e.target;
 
-    setState({
-      ...state,
+    setPersonalInfo({
+      ...personalInfo,
       [name]: value,
     });
   };
 
-  const resetForms = () => setState(initialState);
+  const addToEducationList = () => {
+    const newEducationFormItem = {
+      ...eduFormListInitialValue[0],
+      id: uuid(),
+    };
+
+    setEducationFormList([...educationFormList, newEducationFormItem]);
+  };
+
+  const addToWorkXpList = () => {
+    const newWorkXpItem = {
+      ...expFormListInitialValue[0],
+      id: uuid(),
+    };
+    setWorkXpFormList([...workXpFormList, newWorkXpItem]);
+  };
+
+  const onChangeHandler = (e, id) => {
+    const { value, name } = e.target;
+
+    setEducationFormList(
+      educationFormList.map((item) => {
+        if (item.id === id) {
+          return { ...item, [name]: value };
+        } else {
+          return { ...item };
+        }
+      })
+    );
+    setWorkXpFormList(
+      workXpFormList.map((item) => {
+        if (item.id === id) {
+          return { ...item, [name]: value };
+        } else {
+          return { ...item };
+        }
+      })
+    );
+  };
+
+  const removeItemFromLists = (e, id) => {
+    e.preventDefault();
+    const newEducFormList = educationFormList.filter((form) => form.id !== id);
+    const newExpFormList = workXpFormList.filter((form) => form.id !== id);
+    setEducationFormList(newEducFormList);
+    setWorkXpFormList(newExpFormList);
+  };
+
+  const resetForms = () => {
+    setPersonalInfo(personalData);
+    setEducationFormList(eduFormListInitialValue);
+    setWorkXpFormList(expFormListInitialValue);
+  };
 
   return (
     <AppContext.Provider
-      value={{ state, setState, onChangeHandler, year, resetForms }}
+      value={{
+        addToEducationList,
+        personalInfo,
+        personalInfoOnChangeHandler,
+        educationFormList,
+        removeItemFromLists,
+        eduFormListInitialValue,
+        onChangeHandler,
+        resetForms,
+        addToWorkXpList,
+        workXpFormList,
+      }}
     >
       {children}
     </AppContext.Provider>
