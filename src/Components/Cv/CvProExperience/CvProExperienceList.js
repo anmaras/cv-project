cf7ef1c69b23b3e7a133961a0f058feec294d465
Anthony Maras <marasantonis@gmail.com>
2022-08-29 17:56:07 +0300

    add dynamic component

--- src/Components/Cv/CvProExperience/CvProExperienceList.js
+++ src/Components/Cv/CvProExperience/CvProExperienceList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import CvProExperienceItem from './CvProExperienceItem';
 import style from './CvProExperience.module.css';
+import { useGlobalContext } from '../../../Context';
 
 function CvProExperienceList() {
+  const { workXpFormList } = useGlobalContext();
+
   return (
     <section className={style['pro-section']}>
       <h4>Professional Experience</h4>
       <div>
-        <CvProExperienceItem />
+        {workXpFormList.map((item) => {
+          return <CvProExperienceItem key={item.id} {...item} />;
+        })}
       </div>
     </section>
   );
