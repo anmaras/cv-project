3c303e9d44d4637bd08832d7fe0b8af998ae4483
Anthony Maras <marasantonis@gmail.com>
2022-08-29 17:56:20 +0300

    add dynamic component

--- src/Components/Cv/CvEducation/CvEducationList.js
+++ src/Components/Cv/CvEducation/CvEducationList.js
@@ -1,16 +1,17 @@
-import React from 'react';
 import CvEducationItem from './CvEducationItem';
 import style from './CvEducation.module.css';
+import { useGlobalContext } from '../../../Context';
 
 function CvEducationList() {
+  const { educationFormList } = useGlobalContext();
+
   return (
     <section className={style['education-section']}>
       <h4>Education</h4>
       <div className={style['education-list-wrapper']}>
-        <CvEducationItem />
-        <CvEducationItem />
-        <CvEducationItem />
-        <CvEducationItem />
+        {educationFormList.map((item) => {
+          return <CvEducationItem key={item.id} {...item} />;
+        })}
       </div>
     </section>
   );
