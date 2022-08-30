75a4b5f74ab4fbb071c7a06e84c54c0d931b116a
Anthony Maras <marasantonis@gmail.com>
2022-08-30 18:45:34 +0300

    if checkbox active then present is showing

--- src/Components/Cv/CvEducation/CvEducationItem.js
+++ src/Components/Cv/CvEducation/CvEducationItem.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import style from './CvEducation.module.css';
+import { getSelectedObj } from '../../../Utilities';
+import { useGlobalContext } from '../../../Context';
 
-function CvEducationItem({ university, subject, degree, yearFrom, yearTo }) {
+function CvEducationItem({
+  university,
+  subject,
+  degree,
+  yearFrom,
+  yearTo,
+  id,
+}) {
+  const { educationFormList } = useGlobalContext();
   const from = yearFrom ? yearFrom : 'from';
-  const to = yearTo ? yearFrom : 'to';
+  const to = yearTo ? yearTo : 'to';
   const uniName = university ? university : 'University Name';
   const studiesSubject = subject ? subject : 'Studies Subject';
   const uniDegree = degree ? degree : 'Degree';
+  const checkbox = getSelectedObj(educationFormList, id)['checkbox'];
 
   return (
     <div className={style['education-list-item']}>
       <p className={style.period}>
-        <span>{from}</span> - <span>{to}</span>
+        <span>{from}</span> - <span>{checkbox ? 'Present' : to}</span>
       </p>
       <div className={style['list-item-detail']}>
         <p className={style.subject}>{studiesSubject}</p>
--- src/Components/Cv/CvProExperience/CvProExperienceItem.js
+++ src/Components/Cv/CvProExperience/CvProExperienceItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './CvProExperience.module.css';
+import { getSelectedObj } from '../../../Utilities';
 import { useGlobalContext } from '../../../Context';
 
 function CvProExperienceItem({
@@ -9,27 +10,32 @@ function CvProExperienceItem({
   workDescription,
   workingFrom,
   workingTo,
+  id,
 }) {
+  const { workXpFormList } = useGlobalContext();
+  const checkbox = getSelectedObj(workXpFormList, id)['checkbox'];
+
+  const from = workingFrom ? workingFrom : 'YYYY';
+  const to = workingTo ? workingTo : 'YYYY';
+  const companyName = company ? company : 'Company Name';
+  const companyLocation = companyCity ? companyCity : 'City';
+  const jobPosition = position ? position : 'Your position in that company';
+  const workInfo = workDescription
+    ? workDescription
+    : 'A few words about your work on that company';
+
   return (
     <div className={style['company-wrapper']}>
-      <p className={style['company-name']}>
-        {company ? company : 'Company Name'}
-      </p>
-      <p className={style.location}>{companyCity ? companyCity : 'City'}</p>
+      <p className={style['company-name']}>{companyName}</p>
+      <p className={style.location}>{companyLocation}</p>
       <p className={style.period}>
         <span>
-          {workingFrom ? workingFrom : 'YYYY'} -{' '}
-          {workingTo ? workingTo : 'YYYY'}
+          {from}
+          {checkbox ? 'Present' : to}
         </span>
       </p>
-      <p className={style['job-position']}>
-        {position ? position : 'Your position in that company'}
-      </p>
-      <p className={style['job-description']}>
-        {workDescription
-          ? workDescription
-          : 'A few words about your work on that company'}
-      </p>
+      <p className={style['job-position']}>{jobPosition}</p>
+      <p className={style['job-description']}>{workInfo}</p>
     </div>
   );
 }
