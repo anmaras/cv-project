2a5e7016e756560ba98fc2acb47cce15894bc7ed
Anthony Maras <marasantonis@gmail.com>
2022-08-30 18:45:46 +0300

    add checkbox

--- src/Components/Form/FormEducation/FormEducationListItem.js
+++ src/Components/Form/FormEducation/FormEducationListItem.js
@@ -1,28 +1,46 @@
 import style from '../Form.module.css';
+import CheckBox from '../../Ui/CheckBox';
+import { getSelectedObj } from '../../../Utilities';
 import { useGlobalContext } from '../../../Context';
 
 function FormEducationListItem({ id, type, name }) {
   const { removeItemFromLists, onChangeHandler, educationFormList } =
     useGlobalContext();
 
+  const getObject = (array) => {
+    return array.find((item) => item.id === id);
+  };
+
   return (
     <>
       <form className={style.form}>
         {name.map((formItem, index) => {
-          const formObject = educationFormList.find((item) => item.id === id);
+          const inputType =
+            formItem === 'yearFrom' || formItem === 'yearTo' ? 'month' : type;
+          const isActive =
+            formItem === 'yearTo' &&
+            getSelectedObj(educationFormList, id)['checkbox'];
+          const value = getSelectedObj(educationFormList, id)[formItem];
           return (
             <input
               key={index}
-              type={type}
+              type={inputType}
               name={formItem}
+              disabled={isActive}
               placeholder={formItem}
               onChange={(e) => {
                 onChangeHandler(e, id);
               }}
-              value={formObject[formItem]}
+              value={value}
             />
           );
         })}
+        <CheckBox
+          id={id}
+          getObj={getSelectedObj}
+          list={educationFormList}
+          onChange={onChangeHandler}
+        />
         <button
           onClick={(e) => {
             removeItemFromLists(e, id);
