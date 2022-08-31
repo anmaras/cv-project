21bfa15a197f0a577862738a774a504471c2a4ff
Anthony Maras <marasantonis@gmail.com>
2022-08-31 18:02:51 +0300

    add classname for parent div

--- src/Components/Form/FormEducation/FormEducationList.js
+++ src/Components/Form/FormEducation/FormEducationList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import FormEducationListItem from './FormEducationListItem';
 import { useGlobalContext } from '../../../Context';
+import style from '../Form.module.css';
+
 function FormEducationList() {
   const { educationFormList, addToEducationList } = useGlobalContext();
   return (
-    <div>
+    <div className={style['form-wrapper']}>
       <h3>Education</h3>
       {educationFormList.map((formItem) => {
         return <FormEducationListItem key={formItem.id} {...formItem} />;
