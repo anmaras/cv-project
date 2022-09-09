71c9057a85535a24e0b2c8540b5347ceaa546e12
Anthony Maras <marasantonis@gmail.com>
2022-09-09 15:49:24 +0300

    fixed conditionals for extra info section

--- src/Components/Form/FormExperience/FormExperienceExtraInfo/FormExperienceExtraInfo.js
+++ src/Components/Form/FormExperience/FormExperienceExtraInfo/FormExperienceExtraInfo.js
@@ -1,83 +1,76 @@
 import React from 'react';
 import { useGlobalContext } from '../../../../Context';
 import { BsPlusLg } from 'react-icons/bs';
-
+import { FaMinus } from 'react-icons/fa';
 import style from './FormExperienceExtraInfo.module.css';
 
-function FormExperienceExtraInfo({ id, jobSpecifics }) {
+function FormExperienceExtraInfo({ id, jobSpecifics, isSelected }) {
   const {
-    setIsJobInfoActive,
-    isJobInfoActive,
     jobInfoOnChangeHandler,
     removeJobInfoList,
     addToMoreAboutJobInfoList,
+    getExtraInfoId,
   } = useGlobalContext();
+
+  console.log(isSelected);
 
   return (
     <div className={style['extraInfo-container']}>
       <button
         className={[style['add-extraInfo-btn'], 'btn-style-1'].join(' ')}
-        onClick={(e) => {
-          e.preventDefault();
-          setIsJobInfoActive(!isJobInfoActive);
+        onClick={() => {
+          getExtraInfoId(id);
         }}
       >
-        <BsPlusLg />
+        {isSelected ? <FaMinus /> : <BsPlusLg />}
         add extra info
       </button>
-      {isJobInfoActive && (
+      {isSelected && (
         <fieldset>
           <legend>Extra Info</legend>
-
           <div>
-            {isJobInfoActive && (
-              <>
-                <ul className={style['expJobExtraInfo-list']}>
-                  {jobSpecifics.map((item) => {
-                    return (
-                      <li
-                        key={item.id}
-                        className={style['expJobExtraInfo-listItem']}
-                      >
-                        <div>
-                          <label htmlFor="textArea">Add</label>
-                          <textarea
-                            id="textArea"
-                            placeholder="e.g. Say something about your role in that job..."
-                            name="jobSpecifics"
-                            onChange={(e) => {
-                              jobInfoOnChangeHandler(e, item.id, id);
-                            }}
-                            value={item.info}
-                          />
-                        </div>
-                        <button
-                          className={[
-                            style['remove-info-btn'],
-                            'btn-style-1',
-                          ].join(' ')}
-                          onClick={(e) => {
-                            e.preventDefault();
-                            removeJobInfoList(item.id);
-                          }}
-                        >
-                          remove
-                        </button>
-                      </li>
-                    );
-                  })}
-                </ul>
-                <button
-                  className={[style['add-more-btn'], 'btn-style-1'].join(' ')}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    addToMoreAboutJobInfoList(id);
-                  }}
-                >
-                  <BsPlusLg /> add more
-                </button>
-              </>
-            )}
+            <ul className={style['expJobExtraInfo-list']}>
+              {jobSpecifics.map((item) => {
+                return (
+                  <li
+                    key={item.id}
+                    className={style['expJobExtraInfo-listItem']}
+                  >
+                    <div>
+                      <label htmlFor="textArea">Add</label>
+                      <textarea
+                        id="textArea"
+                        placeholder="e.g. Say something about your role in that job..."
+                        name="jobSpecifics"
+                        onChange={(e) => {
+                          jobInfoOnChangeHandler(e, item.id, id);
+                        }}
+                        value={item.info}
+                      />
+                    </div>
+                    <button
+                      className={[style['remove-info-btn'], 'btn-style-1'].join(
+                        ' '
+                      )}
+                      onClick={() => {
+                        removeJobInfoList(item.id);
+                      }}
+                    >
+                      remove
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+            <button
+              className={[style['add-more-btn'], 'btn-style-1'].join(' ')}
+              onClick={(e) => {
+                e.preventDefault();
+                addToMoreAboutJobInfoList(id);
+              }}
+            >
+              <BsPlusLg /> add more
+            </button>
           </div>
         </fieldset>
       )}
