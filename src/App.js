2db39d68f510c16bdb7462dadddc455d4b4296f5
Anthony Maras <marasantonis@gmail.com>
2022-09-19 12:14:11 +0300

    add print fn for cv component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import Cv from './Components/Cv/Cv';
+import { useRef } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useReactToPrint } from 'react-to-print';
 import FormEducationList from './Components/Form/FormEducation/FormEducationList';
 import FormProExperienceList from './Components/Form/FormExperience/FormProExperienceList';
 import FormPersonalInfo from './Components/Form/FormPersonalInfo/FormPersonalInfo';
 import Form from './Components/Form/Form';
+import Cv from './Components/Cv/Cv';
 import { FaGithub } from 'react-icons/fa';
 import './index.css';
 
 function App() {
   const year = new Date().getFullYear();
+  const componentRef = useRef(null);
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   return (
     <>
       <header>
@@ -24,7 +31,10 @@ function App() {
             </Route>
           </Routes>
         </BrowserRouter>
-        <Cv />
+        <Cv ref={componentRef} />
+        <button className={`print-btn btn-style-2`} onClick={handlePrint}>
+          PRINT
+        </button>
       </main>
       <footer>
         <h2>Anthony Maras </h2>
